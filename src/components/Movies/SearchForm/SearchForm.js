364323ada89495057db28d58d647b06f1b254bec
Natalia Manzhikova <manzhikova-natalia@yandex.ru>
2023-06-21 17:06:33 +0300

    поля формы заблокированы во время отправки запросов

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,30 +1,37 @@
-import React from 'react';
-
-import { useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { useFormWithValidation } from "../../../utils/validation";
 
 function SearchForm({ handleSubmitForm, handleCheckbox, isChecked, setIsChecked }) {
-
   const location = useLocation().pathname;
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const { values, handleChange, errors, isValid } = useFormWithValidation();
 
-  function handleSubmitSeachForm(e) {
+  const handleSubmitSearchForm = async (e) => {
     e.preventDefault();
 
     if (!isValid) {
       console.log(errors);
-    } else {
-      handleSubmitForm({ request: values.search__input });
+      return;
     }
-  }
+
+    try {
+      setIsSubmitting(true);
+      await handleSubmitForm({ request: values.search__input });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   React.useEffect(() => {
-    if (location === '/movies' && localStorage.getItem('searchedText')) {
-      const searchedText = localStorage.getItem('searchedText');
+    if (location === "/movies" && localStorage.getItem("searchedText")) {
+      const searchedText = localStorage.getItem("searchedText");
       values.search__input = searchedText;
-  }
+    }
 
-    if (localStorage.getItem('checkboxMovies') === 'true') {
+    if (localStorage.getItem("checkboxMovies") === "true") {
       setIsChecked(true);
     } else {
       setIsChecked(false);
@@ -33,7 +40,7 @@ function SearchForm({ handleSubmitForm, handleCheckbox, isChecked, setIsChecked 
 
   return (
     <section className="searchform">
-      <form className="searchform__search-block" name='search__form' onSubmit={handleSubmitSeachForm}>
+      <form className="searchform__search-block" name="search__form" onSubmit={handleSubmitSearchForm}>
         <fieldset className="searchform__input-container">
           <input
             type="text"
@@ -43,27 +50,20 @@ function SearchForm({ handleSubmitForm, handleCheckbox, isChecked, setIsChecked 
             name="search__input"
             onChange={handleChange}
             minLength="1"
-            value={values.search__input || ''}
+            value={values.search__input || ""}
+            disabled={isSubmitting} 
           />
         </fieldset>
         <fieldset className="searchform__button-container">
-          <button
-            className={!isValid ? 'searchform__button_error' : 'searchform__button'}
-            type="submit"
-          >
+          <button className={!isValid ? "searchform__button_error" : "searchform__button"} type="submit" disabled={isSubmitting}> 
+            
           </button>
         </fieldset>
       </form>
 
       <fieldset className="filtercheckbox__checkbox-container">
         <label className="filtercheckbox__checkbox-label">
-          <input
-            type="checkbox"
-            name="checkbox"
-            id="checkbox"
-            checked={isChecked}
-            onChange={handleCheckbox}
-          />
+          <input type="checkbox" name="checkbox" id="checkbox" checked={isChecked} onChange={handleCheckbox} disabled={isSubmitting} /> 
           <div className="filtercheckbox__checkbox-slider round"></div>
         </label>
         <label htmlFor="checkbox" className="filtercheckbox__checkbox-labeltext">
